ae80cea1af187cbd2653efd9b8504c4f428a4bd1
MhrDikesh <mhrdikesh137@gmail.com>
2024-03-31 08:15:47 +0545

    changes

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import About from "./features/About";
+import Contact from "./features/Contact";
+import RootLayOut from "./ui/RootLayOut";
+import NotFound from "./ui/NotFound";
+import Home from "./features/Home";
+
+
+const App = () => {
+  // const p = { id: 1, p: 2 }
+  // const person = {
+  //   name: 'per',
+  //   ...p
+  // };
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayOut />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'about', element: <About /> },
+        { path: 'contact', element: <Contact /> },
+        { path: '*', element: <NotFound /> },
+      ]
+    },
+
+
+  ]);
+
+
+
+  return <RouterProvider router={router} />
+}
+export default App
